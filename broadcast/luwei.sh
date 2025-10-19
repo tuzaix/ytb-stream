448ddec2ba35cfdbfe37faee6450828f5d315c25
senc <senchen2013@gmail.com>
2025-10-19 23:23:18 +0800

    fix bug

--- broadcast/luwei.sh
+++ broadcast/luwei.sh
@@ -5,20 +5,14 @@
 bin=`dirname "$0"`
 bin=`cd $bin; pwd`
 
-start_broadcast=$bin/../start-broadcast.sh
-
-category=$1
-timescope=$2
-duration=$3
-thedate=$4
-
 title="🔴 LIVE 🔴 Simmering & Bubbling: ASMR Braising Process! So Satisfying & Mouthwatering."
 
-bash $start_broadcast $category $title $timescope $duration $thedate
+timescope=$1
+duration=$2
+thedate=$3
 
 
-
-
+bash $bin/../start-broadcast.sh "luwei" "$title" "$timescope" "$duration" "$thedate"
 
 
 
--- broadcast/majiang.sh
+++ broadcast/majiang.sh
@@ -5,19 +5,13 @@
 bin=`dirname "$0"`
 bin=`cd $bin; pwd`
 
-start_broadcast=$bin/../start-broadcast.sh
-
-category=$1
-timescope=$2
-duration=$3
-thedate=$4
-
 title="🔴 LIVE 🔴 血戰到底！挑戰八方牌友，今夜決勝負！"
 
-bash $start_broadcast $category $title $timescope $duration $thedate
+timescope=$1
+duration=$2
+thedate=$3
 
-
-
+bash $bin/../start-broadcast.sh "tetris" "$title" "$timescope" "$duration" "$thedate"
 
 
 
