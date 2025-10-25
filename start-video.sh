217ca424487d4268ac6d319676e6276de56dc3f1
senc <senchen2013@gmail.com>
2025-10-25 23:38:51 +0800

    fix bug

---
+++ start-video.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+bin=`dirname "$0"`
+bin=`cd $bin; pwd`
+
+source $bin/../.venv/bin/activate
+
+CHOICE=$1
+TITLE=$2
+DESCRIPTION=$3
+
+if [ -z "$CHOICE" ] || [ -z "$TITLE" ] || [ -z "$DESCRIPTION" ]; then
+    echo "错误：缺少必要参数."
+    echo "用法：$0 <类别> <视频标题> <视频说明>"
+    echo "示例：$0 WokStar \"视频标题\" \"视频说明\""
+    exit 1
+fi
+
+BASE_DIR=/home/ftpuser_hostinger/files
+
+auth_base_dir=$BASE_DIR/auth_impt/$CHOICE
+video_base_dir=$BASE_DIR/video/$CHOICE
+
+OPTS_VIDEO_DIRS=()
+
+while IFS= read -r -d '' sub_dir; do
+    if [[ $sub_dir == $video_base_dir ]]; then
+        continue
+    fi  
+
+    # 假设 $video_base_dir 已经设置
+    if [[ "$sub_dir" == *_published ]]; then
+        continue
+    fi  
+    echo "$sub_dir" # 建议打印时也加引号
+
+    OPTS_VIDEO_DIRS+=("$sub_dir")
+done < <(find "$video_base_dir" -maxdepth 1 -type d -print0)
+#  ^              ^
+#  | 告诉 while 从这里获取输入
+#  | 使用进程替换 <(...) 来执行 find，并将 find 的输出重定向到 while 循环的标准输入
+
+echo "video_dirs: ${OPTS_VIDEO_DIRS[@]}"
+
+python $bin/../upload_video.py --auth_dir $auth_base_dir \
+                                --video_dirs "${OPTS_VIDEO_DIRS[@]}" \
+                                --title "$TITLE" \
+                                --description "$DESCRIPTION" \
+                                --publish
--- video/WokStar.sh
+++ video/WokStar.sh
@@ -2,68 +2,7 @@
 bin=`dirname "$0"`
 bin=`cd $bin; pwd`
 
-source $bin/../.venv/bin/activate
+TITLE=$1
+DESCRIPTION=$2
 
-
-# usage: upload_video.py [-h] --auth_dir AUTH_DIR --video_dirs VIDEO_DIRS [VIDEO_DIRS ...] --title TITLE --description DESCRIPTION [--privacy PRIVACY]
-#                        [--thumbnail THUMBNAIL] [--publish] [--tags TAGS]
-
-# Upload a video to YouTube.
-
-# options:
-#   -h, --help            show this help message and exit
-#   --auth_dir AUTH_DIR   Directory containing client_secrets.json and token.json.
-#   --video_dirs VIDEO_DIRS [VIDEO_DIRS ...]
-#                         Directory containing video files.
-#   --title TITLE         The title of the video.
-#   --description DESCRIPTION
-#                         The description of the video.
-#   --privacy PRIVACY     The privacy status of the video (e.g., private, public, unlisted)
-#   --thumbnail THUMBNAIL
-#                         The path to the thumbnail image
-#   --publish             Publish the video after processing
-#   --tags TAGS           A comma-separated list of tags for the video
-
-
-CHOICE=$1
-TITLE=$2
-DESCRIPTION=$3
-
-if [ -z "$CHOICE" ] || [ -z "$TITLE" ] || [ -z "$DESCRIPTION" ]; then
-    echo "错误：缺少必要参数."
-    echo "用法：$0 <类别> <视频标题> <视频说明>"
-    echo "示例：$0 WokStar \"视频标题\" \"视频说明\""
-    exit 1
-fi
-
-BASE_DIR=/home/ftpuser_hostinger/files
-
-auth_base_dir=$BASE_DIR/auth_impt/$CHOICE
-video_base_dir=$BASE_DIR/video/$CHOICE
-
-
-sub_dirs=$(ls $video_base_dir)
-OPTS_VIDEO_DIRS=""
-
-for sub_dir in $sub_dirs
-do
-    # 判断不是以_published未结尾的文件夹
-    if [[ $sub_dir != *_published ]]; then
-        # 判断改目录下是否有视频文件
-        video_files=$(ls $video_base_dir/$sub_dir/*.mp4)
-        if [ -z "$video_files" ]; then
-            echo "警告：目录 $video_base_dir/$sub_dir 下没有视频文件，将被跳过."
-            continue
-        fi
-        OPTS_VIDEO_DIRS+="\"$video_base_dir/$sub_dir\" "
-    fi
-done
-
-echo "video_dirs: $OPTS_VIDEO_DIRS"
-
-python $bin/../upload_video.py --auth_dir $auth_base_dir \
-                                --video_dirs $OPTS_VIDEO_DIRS \
-                                --title "$TITLE" \
-                                --description "$DESCRIPTION" \
-                                --publish
-
+bash $bin/../start-video.sh "WokStar" "$TITLE" "$DESCRIPTION"
