db93bb73d458b8ee024f4ab85391c88a23eca769
senc <senchen2013@gmail.com>
2025-10-27 00:23:23 +0800

    fg

--- video/SnackFoodHacker.sh
+++ video/SnackFoodHacker.sh
@@ -2,7 +2,7 @@
 bin=`dirname "$0"`
 bin=`cd $bin; pwd`
 
-TITLE="#cookingshorts #chinesefood #delicious #shorts #viral"
-DESCRIPTION="#cookingshorts #chinesefood #delicious #shorts #viral"
+TITLE="Trendy Chinese Street Fashion: Beautiful Girl Outfits & Styles 🔥🥀#shorts #viral"
+DESCRIPTION="Every day, you'll catch stunning Chinese girls rocking incredible outfits that will leave you in awe. If you're loving these videos, hit that like button, drop a quick comment, and share to give me a boost—helping me create even better fashion content for you. Thank you all so much!"
 
 bash $bin/../start-video.sh "SnackFoodHacker" "$TITLE" "$DESCRIPTION"
