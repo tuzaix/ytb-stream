05c9e899c2ddce568056a5383f6fa575d3e8d100
senc <senchen2013@gmail.com>
2025-10-28 01:09:03 +0800

    fg

---
+++ video/start-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+bin=`dirname "$0"`
+bin=`cd $bin; pwd`
+
+bash $bin/TasteofChinaStreets.sh
+bash $bin/StreetFoodSecrets.sh
+bash $bin/QuickSnackMasters.sh
+bash $bin/WokStar.sh
+bash $bin/EasyTastyRecipes.sh
+bash $bin/SnackFoodHacker.sh
+bash $bin/QuickFlavorRecipes.sh
+bash $bin/StreetFoodHacker.sh
