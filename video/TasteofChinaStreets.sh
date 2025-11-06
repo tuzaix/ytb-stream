2b49e69d318f749245466539f53033c8ad4bf051
senc <senchen2013@gmail.com>
2025-11-07 00:46:27 +0800

    fg

--- video/TasteofChinaStreets.sh
+++ video/TasteofChinaStreets.sh
@@ -2,7 +2,7 @@
 bin=`dirname "$0"`
 bin=`cd $bin; pwd`
 
-TITLE="#cookingshorts #chinesefood #delicious #shorts #viral"
-DESCRIPTION="#cookingshorts #chinesefood #delicious #shorts #viral"
+TITLE="Gái Trung Quốc xinh đẹp|Gái nóng bỏng|Douyin #outfits #style"
+DESCRIPTION="Hello everyone, I’m Vẻ Đẹp Phố Phường.Welcome to my channel 💕 ☘️ Thời trang đường phố đang làm mưa làm gió! Cùng Vẻ Đẹp Phố Phường khám phá những bộ trang phục độc đáo và phong cách thời thượng ngay trên phố. Từ những set đồ cá tính, bụi bặm đến phong cách thời trang cao cấp, mọi ánh nhìn đều đổ dồn về những bộ trang phục này. Đừng bỏ lỡ những xu hướng thời trang mới nhất và những cách mix & match sáng tạo để bạn trở thành tâm điểm trong mọi khung hình. 💕 #koreanfashion #trendingvideo #couples #chinastreetfashion #streetstyle #trending #streetfashionchina #TikTokfashion #DouyinChina #TikTokChina #fashion #chinafashion #viral #beautiful 👉 Click Like, Share and Subscribe to not miss any videos from Vẻ Đẹp Phố Phường 💚"
 
 bash $bin/../start-video.sh "TasteofChinaStreets" "$TITLE" "$DESCRIPTION"
