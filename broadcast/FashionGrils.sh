aacefb3e77a690724da7f5801256ef9f9666e8e0
senc <tuzaixchan@gmail.com>
2026-01-11 01:19:20 +0800

    增加fashiongirls

---
+++ broadcast/FashionGrils.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+#卤味直播
+
+bin=`dirname "$0"`
+bin=`cd $bin; pwd`
+
+title="🔴 LIVE 🔴 Beautiful Girls Street Style | Chinese Street Fashion"
+description="#trendingfashion #viralvideos #chinesefashion Beautiful Girls Street Style|Chinese Street Fashion #douyin #Douyin #trendingfashion #viralvideos #chinesefashion"
+
+
+# # 参数
+# CATEGORY=$1    # 类型：cook, tetris等
+# THETITLE=$2    # 直播标题
+# TIMESCOPE=$3  # 时间点：7:00~9:30(2.5h), 10:00~13:30(3.5h), 22:00~次1:30(3.5h), 次2:00~次3:30(2.5h)
+# DURATION=$4    # 指定直播时长，默认是2.5h
+# RUN_DATE=$5    # 指定天的素材: 2025-10-20
+# DESCRIPTION=$6        # 直播说明
+
+timescope=$1
+duration=$2
+thedate=$3
+
+
+bash $bin/../start-broadcast.sh "FashionGrils" "$title" "$timescope" "$duration" "$thedate" "$description"
+
+
+
+
+
+
+
+
+
